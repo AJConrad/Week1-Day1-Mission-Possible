3435fa3130aed6fa06a1709d52c14b85c6c829d8
Andrew Conrad <Andrew@andrews-macbook.apollosrc.root>
2016-04-11 14:54:44 -0400

    First Draft with Optional min reqs

--- MissionPossible/ViewController.m
+++ MissionPossible/ViewController.m
@@ -10,18 +10,31 @@
 
 @interface ViewController ()
 
+@property (nonatomic, weak) IBOutlet UITextField *enterAgentIdentity;
+@property (nonatomic, weak) IBOutlet UITextView  *textView;
+
 @end
 
 @implementation ViewController
 
+# pragma mark - Interactivity
+
+- (IBAction)acceptButtonPressed:(id)button {
+
+    _textView.text = [NSString stringWithFormat:@"%@, your mission, should you choose to accept it, is to enter the vault. Once inside, there are 3 security systems in operation. Disable each system in order from left to right in order to save the world. This message will self destruct in 15 seconds. Good luck %@!",_enterAgentIdentity.text,_enterAgentIdentity.text];
+
+}
+
+# pragma mark - Life Cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
 }
 
 @end
